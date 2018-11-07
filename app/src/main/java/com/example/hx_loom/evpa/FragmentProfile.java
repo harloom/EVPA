48a7fe932f5b747b8553ce374432062edaa327d5
Restu <restusaputraxxxayu@gmail.com>
2018-11-07 17:59:35 +0700

    Bang Restu Profile V 0.0.2

--- app/src/main/java/com/example/hx_loom/evpa/FragmentProfile.java
+++ app/src/main/java/com/example/hx_loom/evpa/FragmentProfile.java
@@ -1,16 +1,32 @@
 package com.example.hx_loom.evpa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class FragmentProfile extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_profile_fragment, container, false);
+        View profileF = inflater.inflate(R.layout.activity_profile_fragment, container, false);
+
+        final LinearLayout v_goBack = (LinearLayout) profileF.findViewById(R.id.backfromAbout);
+        v_goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gg = new Intent(v_goBack.getContext(), AboutActivity.class);
+                startActivity(gg);
+
+            }
+        });
+
+        return  profileF;
     }
+
+
 }
