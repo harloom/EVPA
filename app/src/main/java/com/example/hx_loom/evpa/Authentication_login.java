27118e1f06a2f822de02fbf13e1fdfb159c6db0c
Harloom <ilhamsolehudin1999@gmail.com>
2018-11-10 00:47:36 +0700

    Test Connection database

--- app/src/main/java/com/example/hx_loom/evpa/Authentication_login.java
+++ app/src/main/java/com/example/hx_loom/evpa/Authentication_login.java
@@ -1,31 +1,45 @@
 package com.example.hx_loom.evpa;
 
 import android.inputmethodservice.Keyboard;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Authentication_login extends BottomSheetDialogFragment {
+
 
     public Authentication_login() {
     }
 
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View authentiactionLogin =  inflater.inflate(R.layout.fragment_authentication_login, container, false);
-        return  authentiactionLogin ;
+
+
+
+        View authentiactionLogin = inflater.inflate(R.layout.fragment_authentication_login, container, false);
+        return authentiactionLogin;
     }
+
 
 
 }
--- app/src/main/java/com/example/hx_loom/evpa/MainActivity.java
+++ app/src/main/java/com/example/hx_loom/evpa/MainActivity.java
@@ -6,27 +6,52 @@ import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.hx_loom.evpa.Adapater.PagerAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        listenLogin();
         //
         tabMenu();
 
 
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
 
@@ -93,12 +118,66 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void actionAdd(View view) {
+        if(mAuth.getCurrentUser().getUid().isEmpty()){
+            View fragmentAuth = getLayoutInflater().inflate(R.layout.fragment_authentication_login, null);
+            BottomSheetDialog dialog = new BottomSheetDialog(this);
+            dialog.setContentView(fragmentAuth);
+            dialog.show();
+        }
 
-        View fragmentAuth = getLayoutInflater().inflate(R.layout.fragment_authentication_login, null);
-        BottomSheetDialog dialog = new BottomSheetDialog(this);
-        dialog.setContentView(fragmentAuth);
-        dialog.show();
 
 
+
+
+    }
+
+    public void action_login(View view) {
+         EditText form_username = (EditText) view.findViewById(R.id.form_username);
+         EditText form_password =  (EditText) view.findViewById(R.id.form_password);
+        String email = "ilhamwork19@gmail.com";
+        toastMessage("Email : " +email);
+        String pass = "nokiax6";
+        toastMessage("pass : " +pass);
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("tes", "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    toastMessage("nama : " +user.getUid());
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(String.valueOf(MainActivity.this), "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+    }
+
+    private void toastMessage(String s) {
+        Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
+    }
+
+    private void listenLogin(){
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("Succes", "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d("Error", "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+                // ...
+            }
+        };
     }
 }
